135be3cad9b8d5a3c493677e442fe48eacd6e1cb
Petr Portnov <me@progrm-jarvis.ru>
2024-05-12 15:38:38 +0300

    build: upgrade to Rust `1.78.0`

--- flake.nix
+++ flake.nix
@@ -4,14 +4,38 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
-        rustVersion =
-          (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml);
-      in { devShell = pkgs.mkShell { buildInputs = [ rustVersion ]; }; });
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            rust
+            pkgs.nixfmt-rfc-style
+          ];
+        };
+      }
+    );
 }

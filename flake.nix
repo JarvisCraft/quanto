694358bd41de64c1101692f4e21facee6f7b5366
Petr Portnov <mrjarviscraft@gmail.com>
2024-12-01 23:05:18 +0300

    build: update to Rust 1.83

--- flake.nix
+++ flake.nix
@@ -3,39 +3,41 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
   outputs =
-    {
-      self,
+    inputs@{
+      flake-parts,
       nixpkgs,
-      flake-utils,
       rust-overlay,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        { system, pkgs, ... }:
+        let
+          rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
+          formatter = pkgs.nixfmt-rfc-style;
+          devShells.default = pkgs.mkShell { packages = [ rust ]; };
         };
-        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            rust
-            pkgs.nixfmt-rfc-style
-          ];
-        };
-      }
-    );
+    };
 }
